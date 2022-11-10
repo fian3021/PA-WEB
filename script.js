8790fff5dc43ef6a75bcbb889dd59b2aeb12091b
rmdluy <93176348+rmdluy@users.noreply.github.com>
2022-11-10 15:03:39 +0800

    go go

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ const nav = document.querySelector('.menu-1');
 menuToggle.addEventListener('click', function(){
     nav.classList.toggle('slide');
 });
+
+const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+    //   js code to show/hide password and change icon
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
